4828a77a508af121a6a05d175b2f35e22f568a52
unknown <henryhe9999@gmail.com>
2018-04-07 12:36:41 -0400

    Initial setup

---
+++ BlockchainDemo/BlockchainDemo/Block.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockchainDemo
+{
+    public class Block
+    {
+        int _index;
+        DateTime _timeStamp;
+        string _data;
+        string _previousHash;
+        string _hash;
+        int _nonce = 0;
+
+        public int Index
+        {
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                _index = value;
+            }
+        }
+
+        public string PreviousHash
+        {
+            get
+            {
+                return _previousHash;
+            }
+            set
+            {
+                _previousHash = value;
+            }
+        }
+        public string Hash
+        {
+            get
+            {
+                return _hash;
+            }
+            set
+            {
+                _hash = value;
+            }
+        }
+
+        public string Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+            }
+        }
+
+        public DateTime Timstamp
+        {
+            get
+            {
+                return _timeStamp;
+            }
+        }
+
+        public Block(DateTime timeStamp, string data)
+        {
+            _index = 0;
+            _timeStamp = timeStamp;
+            _data = data;
+            _previousHash = null;
+            _hash = null;
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{_index}-{_timeStamp}-{_data}-{_previousHash??""}-{_nonce}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+
+
+
+    }
+}
---
+++ BlockchainDemo/BlockchainDemo/Blockchain.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockchainDemo
+{
+    public class Blockchain
+    {
+        IList<Block> _chain = new List<Block>();
+
+        public IList<Block> Chain
+        {
+            get
+            {
+                return _chain;
+            }
+        }
+
+        public Blockchain()
+        {
+            _chain.Add(CreateGenesisBlock());
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            Block block = new Block(DateTime.Now, "Genesis Block");
+            block.Hash = block.CalculateHash();
+
+            return block;
+        }
+
+        public void AddBlock(Block block)
+        {
+            if (_chain.Count > 0)
+            {
+                block.Index = _chain[_chain.Count - 1].Index + 1;
+                block.PreviousHash = _chain[_chain.Count - 1].Hash;
+                block.Hash = block.CalculateHash();
+            }
+            _chain.Add(block);
+        }
+
+        public bool Validate()
+        {
+            for (int i = 1; i < _chain.Count; i++)
+            {
+                Block currentBlock = _chain[i];
+                Block previousBlock = _chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
